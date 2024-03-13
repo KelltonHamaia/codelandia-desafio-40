8a521ac07b9aabd0f272f3733969014f75411657
kellton hamaia <hamaia.kell@gmail.com>
2024-03-13 16:17:11 -0300

    fix: fix background-color of chart-item component

--- src/components/chart/chart-item.tsx
+++ src/components/chart/chart-item.tsx
@@ -7,8 +7,8 @@ type Props = {
 export const Chartitem = ({ title, children }: Props) => {
 
     return (
-        <div className="h-full max-h-[363px] py-8 bg-indigo-50 grid-cols-1 px-4 rounded">
-            <h1 className='text-xl tracking-tight font-semibold my-2 ml-2 text-brand'>{title}</h1>
+        <div className="h-full max-h-[363px] py-8 bg-indigo-50 dark:bg-transparent dark:border dark:rounded-lg dark:border-zinc-700 grid-cols-1 px-4 rounded my-2">
+            <h1 className='text-xl tracking-tight font-semibold my-2 ml-2 text-indigo-600'>{title}</h1>
             {children}
         </div>
     );
--- src/components/chart/chartArea.tsx
+++ src/components/chart/chartArea.tsx
@@ -1,8 +1,11 @@
 import { Chartitem } from "@/components/chart/chart-item";
 import { IncomeChart } from "@/components/chart/income-chart";
 import { TransactionHistory } from "./transaction-history";
+import { useState } from "react";
 
 export const ChartArea = () => {
+
+    const [] = useState(false);
 
     return (
         <div className='w-full grid grid-cols-1 lg:grid-cols-2 gap-8'>
